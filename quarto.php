004e19bf41ea2c9fd77cd842aefc4a3186046e21
Nayra-pixel <nayrasduarte@gmail.com>
2025-03-31 20:21:01 -0300

    exercício 4

--- quarto.php
+++ quarto.php
@@ -1,8 +1,26 @@
 <?php 
-	$num1 = 7;
+	$num1 = 8;
 	$num2 = 9;
-	$num3 = 8;
+	$num3 = 7;
+	$cont = 0;
+	$t = 0;
 
-	if()
+	while($cont <= 4){
 
+		if($num1 > $num2){
+           $t = $num1;
+           $num1 = $num2;
+           $num2 = $t;
+
+       }else if($num2 > $num3){
+       	$t = $num2;
+       	$num2 = $num3;
+       	$num3 = $t;
+       }
+
+       $cont++;
+   }
+
+   echo($num3 . "<br>" . $num2 . "<br>" . $num1);
+   
  ?>
