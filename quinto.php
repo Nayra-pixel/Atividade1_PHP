359930cf4e17a8e0e27c326e20d3e9f8a56b62be
Nayra-pixel <nayrasduarte@gmail.com>
2025-04-01 22:13:29 -0300

    quinto php

--- quinto.php
+++ quinto.php
@@ -1,5 +1,11 @@
 <?php 
+	$altura = 0;
+	$idade = 0;
+	$sexo = "F";
 
+	for ($x = 0; $x <= 50; $x++){
+		echo ("A altura é ". $altura . ", a idade é " . $idade . " e o sexo é " . $sexo . $x . "<br/>");
+	}
 
 
 ?>
